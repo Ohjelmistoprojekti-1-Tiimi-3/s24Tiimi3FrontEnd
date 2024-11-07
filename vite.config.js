33931e04340100c7b04fad2b1251426b75b95c93
bhi021@myy.haaga-helia.fi <bhi021@myy.haaga-helia.fi>
2024-11-07 15:11:22 +0200

    typo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/s24Tiimi3FrontEnd/",
+  base: "https://ohjelmistoprojekti-1-tiimi-3.github.io/s24Tiimi3FrontEnd/",
   plugins: [react()],
 })
