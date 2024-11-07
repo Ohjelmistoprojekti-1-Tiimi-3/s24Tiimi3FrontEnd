aa8fda862ec759fc41b501a944fdb0edfa9d717e
bhi021@myy.haaga-helia.fi <bhi021@myy.haaga-helia.fi>
2024-11-07 15:16:13 +0200

    revert

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://ohjelmistoprojekti-1-tiimi-3.github.io/s24Tiimi3FrontEnd/",
+  base: "/s24Tiimi3FrontEnd/",
   plugins: [react()],
 })
