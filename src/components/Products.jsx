3cc4203679683cf05d79380122f1bbf6f0f51c18
bhi021@myy.haaga-helia.fi <bhi021@myy.haaga-helia.fi>
2024-11-17 18:41:44 +0200

    added product fetching from backend

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,43 @@
+import { AgGridReact } from "ag-grid-react";
+import { useEffect, useState } from "react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-material.css";
+
 export default function Products() {
+
+    const [products, setProducts] = useState([{ productname: "", price: "", color: "", info: "" }]);
+    const [colDefs] = useState([
+        { field: 'productname' },
+        { field: 'price' },
+        { field: 'color' },
+        { field: 'info' }
+    ]);
+
+    const getProducts = () => {
+        fetch("https://tiimi-3-back-end-tiimi3-backend.2.rahtiapp.fi/api/products", { method: 'GET' })
+        .then(response => {
+            console.log("response: ", response);
+            return response.json();
+        })
+        .then(data => {
+            console.log("data ", data);
+            setProducts(data);
+        })
+        .catch(err => {
+            console.error(err.data);
+        });
+    };
+
+    useEffect(() => getProducts(), []);
+
     return (
-        <h3>Työnalla...</h3>
+        <>
+        <div className="ag-theme-material" style={{ width: 900, height: 500 }}>
+            <AgGridReact 
+                rowData={products}
+                columnDefs={colDefs}
+            />
+        </div>
+        </>
     );
 }
