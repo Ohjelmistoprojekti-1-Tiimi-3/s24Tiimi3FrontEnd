defaa8ff4f0b5859403142153671cca8a7e43f6c
Jonna Rinne <jonna.rinne1@gmail.com>
2024-11-24 15:30:55 +0200

    updated About-page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,26 @@
-export default function About() {
-    return (
-        <>
-            <h3>Tietoa meistä</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quidem alias, placeat voluptatum quia tenetur in quod! Dolore officia non, iste accusamus omnis nam eos, incidunt accusantium aspernatur ipsa repellat!</p>
-        </>
-    );
-}
+import React from 'react';
+
+const About = () => {
+  const storeInfo = {
+    name: "Omppu & Rane Koiratarvikkeet",
+    owners: ["Omppu", "Rane"],
+    establishedYear: 2024,
+    businessId: "1234567-8",
+  };
+
+  return (
+    <div style={{ padding: "20px", maxWidth: "600px", margin: "0 auto", fontFamily: "Arial, sans-serif" }}>
+      <h1>{storeInfo.name}</h1>
+      <p><strong>Perustettu:</strong> {storeInfo.establishedYear}</p>
+      <p><strong>Y-tunnus:</strong> {storeInfo.businessId}</p>
+      <h3>Omistajat:</h3>
+      <ul>
+        {storeInfo.owners.map((owner, index) => (
+          <li key={index}>{owner}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default About;
